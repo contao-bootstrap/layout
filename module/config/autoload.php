d6cb60b9e225efba7e32f3f9e3c91e1a698824eb
David Molineus <mail@netzmacht.de>
2014-06-16 23:10:27 +0200

    bug fixes

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -7,8 +7,8 @@
 if(version_compare(VERSION, '3.3', '<')) {
 	\TemplateLoader::addFiles(array(
 		'fe_bootstrap' => 'system/modules/bootstrap-layout/templates/3.2' ,
-		'block_section' => 'system/modules/bootstrap-layout/templates/layout',
-		'block_sections' => 'system/modules/bootstrap-layout/templates/layout',
+		'block_section' => 'system/modules/bootstrap-layout/templates/3.2',
+		'block_sections' => 'system/modules/bootstrap-layout/templates/3.2',
 	));
 }
 else {

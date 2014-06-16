90c830d1622e42844ee190c9815c4af31fd92c31
David Molineus <mail@netzmacht.de>
2014-06-16 22:37:45 +0200

    add fe_bootstrap for Contao 3.3

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -1,13 +1,16 @@
 <?php
 
 \TemplateLoader::addFiles(array(
-	'fe_bootstrap' => 'system/modules/bootstrap-layout/templates',
 	'block_jumbotron' => 'system/modules/bootstrap-layout/templates',
 ));
 
 if(version_compare(VERSION, '3.3', '<')) {
 	\TemplateLoader::addFiles(array(
+		'fe_bootstrap' => 'system/modules/bootstrap-layout/templates/3.2' ,
 		'block_section' => 'system/modules/bootstrap-layout/templates/layout',
 		'block_sections' => 'system/modules/bootstrap-layout/templates/layout',
 	));
+}
+else {
+	\TemplateLoader::addFile('fe_bootstrap', 'system/modules/bootstrap-layout/templates/3.3');
 }

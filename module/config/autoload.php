b4c2ef9f851381d461be6f3189666a06c2aab2a5
David Molineus <mail@netzmacht.de>
2014-07-28 11:24:50 +0200

    rework templates to work with Contaos very limited templates editor

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -4,15 +4,16 @@
 	'block_section_jumbotron' 			=> 'system/modules/bootstrap-layout/templates',
 	'block_section_jumbotron_container' => 'system/modules/bootstrap-layout/templates',
 	'block_section_container' 			=> 'system/modules/bootstrap-layout/templates',
+	'fe_bootstrap' 			            => 'system/modules/bootstrap-layout/templates',
 ));
 
 if(version_compare(VERSION, '3.3', '<')) {
 	\TemplateLoader::addFiles(array(
-		'fe_bootstrap' => 'system/modules/bootstrap-layout/templates/3.2' ,
-		'block_section' => 'system/modules/bootstrap-layout/templates/3.2',
-		'block_sections' => 'system/modules/bootstrap-layout/templates/3.2',
+		'fe_bootstrap_32' => 'system/modules/bootstrap-layout/templates/3.2' ,
+		'block_section'   => 'system/modules/bootstrap-layout/templates/3.2',
+		'block_sections'  => 'system/modules/bootstrap-layout/templates/3.2',
 	));
 }
 else {
-	\TemplateLoader::addFile('fe_bootstrap', 'system/modules/bootstrap-layout/templates/3.3');
+	\TemplateLoader::addFile('fe_bootstrap_33', 'system/modules/bootstrap-layout/templates/3.3');
 }

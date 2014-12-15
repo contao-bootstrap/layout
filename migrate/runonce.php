e7f643c85355c8d1cd30f035e5ea8d3df4e5aca9
David Molineus <mail@netzmacht.de>
2014-12-15 13:48:38 +0100

    fix column

--- migrate/runonce.php
+++ migrate/runonce.php
@@ -6,25 +6,25 @@ class runonce
 {
     public function execute()
     {
-//        \BackendUser::getInstance();
-//        $database = \Database::getInstance();
-//
-//        if (!$database->fieldExists('bootstrap_sections', 'tl_layout')) {
-//            return;
-//        }
-//
-//        // make sure flexible sections exists
-//        if (!$database->fieldExists('flexible_sections', 'tl_layout')) {
-//            $database->query('ALTER TABLE tl_layout ADD flexible_sections blob NULL');
-//        }
-//
-//        $query = <<<SQL
-//UPDATE  tl_layout
-//SET     flexible_sections=bootstrap_section
-//WHERE   bootstrap_section != '' AND flexible_sections = ''
-//SQL;
-//
-//        $database->query($query);
+        \BackendUser::getInstance();
+        $database = \Database::getInstance();
+
+        if (!$database->fieldExists('bootstrap_sections', 'tl_layout')) {
+            return;
+        }
+
+        // make sure flexible sections exists
+        if (!$database->fieldExists('flexible_sections', 'tl_layout')) {
+            $database->query('ALTER TABLE tl_layout ADD flexible_sections blob NULL');
+        }
+
+        $query = <<<SQL
+UPDATE  tl_layout
+SET     flexible_sections=bootstrap_sections
+WHERE   bootstrap_sections != '' AND flexible_sections IS NULL
+SQL;
+
+        $database->query($query);
     }
 }
 

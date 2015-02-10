e0b510469316bcbf1c8cb3cf88755e25e56813cb
David Molineus <david.molineus@netzmacht.de>
2015-02-10 18:48:56 +0100

    Indent by space, not by tabs.

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -10,10 +10,10 @@
  */
 
 \TemplateLoader::addFiles(array(
-        'block_section_jumbotron' 			=> 'system/modules/bootstrap-layout/templates',
+        'block_section_jumbotron'           => 'system/modules/bootstrap-layout/templates',
         'block_section_jumbotron_container' => 'system/modules/bootstrap-layout/templates',
-        'block_section_container' 			=> 'system/modules/bootstrap-layout/templates',
-        'fe_bootstrap' 			            => 'system/modules/bootstrap-layout/templates',
+        'block_section_container'           => 'system/modules/bootstrap-layout/templates',
+        'fe_bootstrap'                      => 'system/modules/bootstrap-layout/templates',
     ));
 
 if (version_compare(VERSION, '3.3', '<')) {

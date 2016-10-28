1bb63f1e37b072ca1ff8d54155bb273e3f1a4490
David Molineus <david.molineus@netzmacht.de>
2016-10-28 16:07:05 +0200

    Use the new modifier implementation.

---
+++ src/DependencyInjection/ContaoBootstrapLayoutExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @package    contao-bootstrap
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2016 netzmacht David Molineus. All rights reserved.
+ * @filesource
+ *
+ */
+
+namespace ContaoBootstrap\Layout\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class ContaoBootstrapLayoutExtension
+ *
+ * @package ContaoBootstrap\Layout\DependencyInjection
+ */
+class ContaoBootstrapLayoutExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ src/View/Template/ReplaceCssClassesModifier.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * @package    contao-bootstrap
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014-2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace ContaoBootstrap\Layout\View\Template;
+
+use ContaoBootstrap\Core\View\Template\AbstractParseModifier;
+
+/**
+ * Class Modifier stores the replace css classes hook.
+ *
+ * @package ContaoBootstrap\Layout\Templates
+ */
+class ReplaceCssClassesModifier extends AbstractParseModifier
+{
+    /**
+     * Css class replacements.
+     *
+     * @var array
+     */
+    private $cssClasses;
+
+    /**
+     * ReplaceCssClassesModifier constructor.
+     *
+     * @param array $templateNames Supported template names.
+     * @param array $cssClasses    Css class replacements.
+     */
+    public function __construct(array $templateNames, array $cssClasses)
+    {
+        parent::__construct($templateNames);
+
+        $this->cssClasses = $cssClasses;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($templateName)
+    {
+        if (empty($this->cssClasses)) {
+            return false;
+        }
+
+        return parent::supports($templateName);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parse($buffer, $templateName)
+    {
+        $classes = array_map(
+            function ($class) {
+                return preg_quote($class, '~');
+            },
+            array_keys($this->cssClasses)
+        );
+
+        $search = sprintf('~class="([^"]*(%s)[^"]*)"~', implode('|', $classes));
+        $buffer = preg_replace_callback(
+            $search,
+            function ($matches) {
+                $classes = explode(' ', $matches[1]);
+                $classes = array_filter($classes);
+
+                foreach ($classes as $index => $class) {
+                    if (array_key_exists($class, $this->cssClasses)) {
+                        $classes[$index] = $this->cssClasses[$class];
+                    }
+                }
+
+                return sprintf('class="%s"', implode(' ', $classes));
+            },
+            $buffer
+        );
+
+        return $buffer;
+    }
+}
